9903cbeba3336a10ab67596c952e62681468b0f4
Alexandr150983 <alexandr150983@gmail.com>
2023-07-28 14:11:55 +0300

    task 1,2,3 final

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,45 @@
+import throttle from "lodash.throttle";
+
+const form = document.querySelector('.feedback-form');
+const emmailInput = form.querySelector('input[name="email"]');
+const messageInput = form.querySelector('textarea[name="message"]');
+
+const saveFormData = throttle(() => {
+    const formData = {
+        emmail: emmailInput.value,
+        message: messageInput.value,
+    }
+    localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+}, 500);
+
+emmailInput.addEventListener('input', saveFormData);
+messageInput.addEventListener('input', saveFormData);
+
+const loadFormData = () => {
+    const storedData = localStorage.getItem('feedback-form-state');
+    if (storedData) {
+        const formData = JSON.parse(storedData);
+        emmailInput.value = formData.emmail || "";
+        messageInput.value = formData.message || "";
+    }
+};
+
+loadFormData();
+
+form.addEventListener('submit', (evt) => {
+    evt.preventDefault();
+    const formData = {
+        emmail: emmailInput.value,
+        message: messageInput.value,
+    }
+
+    console.log(formData);
+    localStorage.removeItem('feedback-form-state');
+    emmailInput.value = '';
+    messageInput.value = '';
+});
+
+
+
+
 
